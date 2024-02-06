b25e47a1d5314bc0d85e8826500bf112a13120cf
cometa2000 <tuxmorro2016@gmail.com>
2024-02-06 10:28:40 -0600

    version 2

--- index.php
+++ index.php
@@ -3,4 +3,32 @@
     $dotenv = Dotenv\Dotenv::createImmutable('./');
     $dotenv->load();
     echo $_ENV['MI_VARIABLE_ENTORNO'];
+    
+    echo "<br>";
+    echo $_ENV['DB_CONNECTION'];
+    echo "<br>";
+    echo $_ENV['DB_HOST'];
+    echo "<br>";
+    echo $_ENV['DB_PORT'];
+    echo "<br>";
+    echo $_ENV['DB_DATABASE'];
+    echo "<br>";
+    echo $_ENV['DB_USERNAME'];
+    echo "<br>";
+    echo $_ENV['DB_PASSWORD'];
+
+    
+    $host = $_ENV['DB_HOST'];
+    $database = $_ENV['DB_DATABASE'];
+    $username = $_ENV['DB_USERNAME'];
+    $password = $_ENV['DB_PASSWORD'];
+    $conexion = new mysqli($host, $username, $password, $database);
+
+    if ($conexion->connect_error) {
+        die("Error de conexión: " . $conexion->connect_error);
+    } else{
+        echo "Conexión exitosa a la base de datos";
+    }
+
+    
 ?>
