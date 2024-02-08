fa4fc2e6a3d69ae1f16937961f96065ff2ec800d
cometa2000 <tuxmorro2016@gmail.com>
2024-02-08 11:46:30 -0600

    Tarea funciones update-delete-add

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
 <?php
-    require_once realpath('./vendor/autoload.php');
-    $dotenv = Dotenv\Dotenv::createImmutable('./');
+    require_once realpath('../vendor/autoload.php');
+    $dotenv = Dotenv\Dotenv::createImmutable('../');
     $dotenv->load();
-    echo $_ENV['MI_VARIABLE_ENTORNO'];
-    
-    echo "<br>";
-    echo $_ENV['DB_CONNECTION'];
-    echo "<br>";
-    echo $_ENV['DB_HOST'];
-    echo "<br>";
-    echo $_ENV['DB_PORT'];
-    echo "<br>";
-    echo $_ENV['DB_DATABASE'];
-    echo "<br>";
-    echo $_ENV['DB_USERNAME'];
-    echo "<br>";
-    echo $_ENV['DB_PASSWORD'];
 
     
     $host = $_ENV['DB_HOST'];
